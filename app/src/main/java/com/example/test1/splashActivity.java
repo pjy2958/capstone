d77dd32b0b3ce0a6228a2831fe23c75f1555afb0
tjd985 <tjd985@gmail.com>
2020-05-31 19:24:36 +0900

    ho

---
+++ app/src/main/java/com/example/test1/splashActivity.java
@@ -0,0 +1,20 @@
+package com.example.test1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
